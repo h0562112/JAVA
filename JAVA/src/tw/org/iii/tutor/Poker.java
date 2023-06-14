1a70e2f3fa8640ed805ecb8ccc6bb80f9b1b4bb9
User <User@WS-13.r1802.tciii.org.tw>
2023-06-14 16:05:56 +0800

    20230614

---
+++ JAVA/src/tw/org/iii/tutor/Poker.java
@@ -0,0 +1,27 @@
+package tw.org.iii.tutor;
+
+public class Poker {
+
+	public static void main(String[] args) {
+		int [] poker = new int[52];
+		for (int i=0;i<poker.length;i++) {
+			int temp = (int)(Math.random()*52);
+			
+			boolean isRepeat = false;
+			for (int j=0;j<i;j++) {
+				if(poker[j] == temp) {
+					
+					isRepeat = true;
+					break;
+				}
+			}
+			if(!isRepeat) {
+			poker[i] = temp;
+			System.out.println(poker[i]);
+			}else {
+				i--;
+			}
+		}
+	}
+
+}
--- JAVA/src/tw/org/iii/tutor/test18.java
+++ JAVA/src/tw/org/iii/tutor/test18.java
@@ -22,6 +22,13 @@ public class test18 {
 		}
 		System.out.println();
 	}
+	System.out.println("--------");
+	for(int i=0;i<a.length;i++) {
+		for(int j=0;i<a[i].length;i++) {
+			System.out.print(a[i][j]+" ");
+		}
+		System.out.println();
+	}
 	}
 
 }
