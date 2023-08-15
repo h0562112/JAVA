63c68dd240dde18b6d8d4604775c93a749b1acb3
User <User@WS-13.r1802.tciii.org.tw>
2023-08-15 16:33:44 +0800

    0815

--- SpringExampleMM22/src/main/java2/lab09/JavaConfig.java
+++ SpringExampleMM22/src/main/java2/lab09/JavaConfig.java
@@ -5,10 +5,12 @@ import java.util.Date;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class JavaConfig {
 	@Bean
+	@Scope("prototype")
     public String now() {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		sdf.setLenient(false);
